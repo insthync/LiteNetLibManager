d5e8238988d7325629761362623a2f5c74d3eb29
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2017-10-23 19:36:21 +0700

    Add demo contents

---
+++ Demo/Scripts/CustomNetworkManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomNetworkManager : LiteNetLibManager {
+    public static CustomNetworkManager Singleton { get; private set; }
+    protected override void Awake()
+    {
+        if (Singleton != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+        Singleton = this;
+        base.Awake();
+    }
+}
