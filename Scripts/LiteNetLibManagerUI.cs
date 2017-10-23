e6fb584ea684305df790d25cfc19c251f28887a2
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2017-10-23 19:35:56 +0700

    Add UI script

---
+++ Scripts/LiteNetLibManagerUI.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LiteNetLibManager))]
+public class LiteNetLibManagerUI : MonoBehaviour
+{
+    public LiteNetLibManager manager;
+    [SerializeField]
+    public bool showGUI = true;
+    [SerializeField]
+    public int offsetX;
+    [SerializeField]
+    public int offsetY;
+
+    void Awake()
+    {
+        manager = GetComponent<LiteNetLibManager>();
+    }
+
+    void OnGUI()
+    {
+        if (!showGUI)
+            return;
+
+        int xpos = 10 + offsetX;
+        int ypos = 40 + offsetY;
+        const int spacing = 24;
+
+        bool noConnection = (manager.client == null);
+
+        if (!manager.IsClient && !manager.IsServer)
+        {
+            if (noConnection)
+            {
+                if (Application.platform != RuntimePlatform.WebGLPlayer)
+                {
+                    if (GUI.Button(new Rect(xpos, ypos, 200, 20), "LAN Host(H)"))
+                    {
+                        manager.StartHost();
+                    }
+                    ypos += spacing;
+                }
+
+                if (GUI.Button(new Rect(xpos, ypos, 105, 20), "LAN Client(C)"))
+                {
+                    manager.StartClient();
+                }
+
+                manager.networkAddress = GUI.TextField(new Rect(xpos + 100, ypos, 95, 20), manager.networkAddress);
+                ypos += spacing;
+
+                if (GUI.Button(new Rect(xpos, ypos, 200, 20), "LAN Server Only(S)"))
+                {
+                    manager.StartServer();
+                }
+                ypos += spacing;
+            }
+            else
+            {
+                GUI.Label(new Rect(xpos, ypos, 200, 20), "Connecting to " + manager.networkAddress + ":" + manager.networkPort + "..");
+                ypos += spacing;
+
+
+                if (GUI.Button(new Rect(xpos, ypos, 200, 20), "Cancel Connection Attempt"))
+                {
+                    manager.StopClient();
+                }
+            }
+        }
+        else
+        {
+            if (manager.IsServer)
+            {
+                string serverMsg = "Server: port=" + manager.networkPort;
+                GUI.Label(new Rect(xpos, ypos, 300, 20), serverMsg);
+                ypos += spacing;
+            }
+            if (manager.IsClient)
+            {
+                GUI.Label(new Rect(xpos, ypos, 300, 20), "Client: address=" + manager.networkAddress + " port=" + manager.networkPort);
+                ypos += spacing;
+            }
+        }
+
+        if (manager.IsServer || manager.IsClient)
+        {
+            if (GUI.Button(new Rect(xpos, ypos, 200, 20), "Stop (X)"))
+            {
+                manager.StopHost();
+            }
+            ypos += spacing;
+        }
+    }
+}
