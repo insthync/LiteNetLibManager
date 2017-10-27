2274b6549079f15eca34aeee9b39cbc895b1865b
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2017-10-27 10:32:39 +0700

    Add more type of sync field

---
+++ Scripts/GameApi/SyncFields/SyncFieldColor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using LiteNetLib.Utils;
+using UnityEngine;
+
+namespace LiteNetLibHighLevel
+{
+    [Serializable]
+    public class SyncFieldColor : LiteNetLibSyncField<Color>
+    {
+        public override bool IsValueChanged(Color newValue)
+        {
+            return !newValue.Equals(value);
+        }
+
+        public override void Deserialize(NetDataReader reader)
+        {
+            value = new Color(reader.GetFloat(), reader.GetFloat(), reader.GetFloat(), reader.GetFloat());
+        }
+
+        public override void Serialize(NetDataWriter writer)
+        {
+            writer.Put(value.r);
+            writer.Put(value.g);
+            writer.Put(value.b);
+            writer.Put(value.a);
+        }
+    }
+}
