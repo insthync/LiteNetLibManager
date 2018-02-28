4283e876b3fd8ec8c70715edc9f95ab536fa15e0
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-02-28 21:50:36 +0700

    Add visible checker codes, will be used to test observer system

---
+++ Scripts/GameApi/LiteNetLibVisibleChecker.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LiteNetLibHighLevel
+{
+    public class LiteNetLibVisibleChecker : LiteNetLibBehaviour
+    {
+        public enum CheckMethod
+        {
+            Physics3D,
+            Physics2D
+        };
+        public int range = 10;
+        public float updateInterval = 1.0f;
+        public CheckMethod checkMethod = CheckMethod.Physics3D;
+
+        private float lastUpdateTime;
+
+        void Update()
+        {
+            if (!IsServer)
+                return;
+
+            if (Time.realtimeSinceStartup - lastUpdateTime > updateInterval)
+            {
+                Identity.RebuildSubscribers(false);
+                lastUpdateTime = Time.realtimeSinceStartup;
+            }
+        }
+
+        public override bool ShouldAddSubscriber(LiteNetLibPlayer subscriber)
+        {
+            var spawnedObjects = subscriber.SpawnedObjects.Values;
+            foreach (var spawnedObject in spawnedObjects)
+            {
+                var pos = spawnedObject.transform.position;
+                if ((pos - transform.position).magnitude < range)
+                    return true;
+            }
+            return false;
+        }
+
+        public override bool OnRebuildSubscribers(HashSet<LiteNetLibPlayer> observers, bool initial)
+        {
+            // find players within range
+            switch (checkMethod)
+            {
+                case CheckMethod.Physics3D:
+                    {
+                        var hits = Physics.OverlapSphere(transform.position, range);
+                        foreach (var hit in hits)
+                        {
+                            var identity = hit.GetComponent<LiteNetLibIdentity>();
+                            if (identity != null && identity.Player != null)
+                                observers.Add(identity.Player);
+                        }
+                        return true;
+                    }
+
+                case CheckMethod.Physics2D:
+                    {
+                        var hits = Physics2D.OverlapCircleAll(transform.position, range);
+                        foreach (var hit in hits)
+                        {
+                            var identity = hit.GetComponent<LiteNetLibIdentity>();
+                            if (identity != null && identity.Player != null)
+                                observers.Add(identity.Player);
+                        }
+                        return true;
+                    }
+            }
+            return false;
+        }
+    }
+}
