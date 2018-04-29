a73a5b0e3d6ce71fa99770e66de7e237f8e99ef8
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-04-30 03:38:43 +0700

    Add LiteNetLibScene

---
+++ Editor/LiteNetLibScenePropertyDrawer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace LiteNetLibHighLevel
+{
+    [CustomPropertyDrawer(typeof(LiteNetLibScene))]
+    public class LiteNetLibScenePropertyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect _position, SerializedProperty _property, GUIContent _label)
+        {
+            EditorGUI.BeginProperty(_position, GUIContent.none, _property);
+            SerializedProperty sceneAsset = _property.FindPropertyRelative("sceneAsset");
+            SerializedProperty sceneName = _property.FindPropertyRelative("sceneName");
+            _position = EditorGUI.PrefixLabel(_position, GUIUtility.GetControlID(FocusType.Passive), _label);
+            if (sceneAsset != null)
+            {
+                EditorGUI.BeginChangeCheck();
+
+                Object value = EditorGUI.ObjectField(_position, sceneAsset.objectReferenceValue, typeof(SceneAsset), false);
+                if (EditorGUI.EndChangeCheck())
+                {
+                    sceneAsset.objectReferenceValue = value;
+                    if (sceneAsset.objectReferenceValue != null)
+                        sceneName.stringValue = (sceneAsset.objectReferenceValue as SceneAsset).name;
+                }
+            }
+            EditorGUI.EndProperty();
+        }
+    }
+}
---
+++ Scripts/Utils/LiteNetLibScene.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace LiteNetLibHighLevel
+{
+    [System.Serializable]
+    public class LiteNetLibScene
+    {
+        [SerializeField]
+        private Object sceneAsset;
+        [SerializeField]
+        private string sceneName = "";
+
+        public string SceneName
+        {
+            get { return sceneName; }
+        }
+
+        public static implicit operator string(LiteNetLibScene unityScene)
+        {
+            return unityScene.SceneName;
+        }
+
+        public bool IsSet()
+        {
+            return !string.IsNullOrEmpty(sceneName);
+        }
+    }
+}
