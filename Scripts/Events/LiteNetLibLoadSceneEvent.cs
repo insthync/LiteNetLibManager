8928ae3e45ede49931cac0fbf413fcf0b927510f
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-06-03 17:51:12 +0700

    Remove unused functions (that generate by unity :p)

--- Scripts/Events/LiteNetLibLoadSceneEvent.cs
+++ Scripts/Events/LiteNetLibLoadSceneEvent.cs
@@ -3,14 +3,4 @@
 [System.Serializable]
 public class LiteNetLibLoadSceneEvent : UnityEvent<string, bool, float>
 {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
