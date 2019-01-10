35469eb4e872778d43c6ae18054f0107cc741e82
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-01-10 12:58:11 +0700

    Move WSBehavior out from WebSocketTransport

---
+++ Scripts/Transports/WebSocket/WSBehavior.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using LiteNetLib.Utils;
+#if !UNITY_WEBGL || UNITY_EDITOR
+using WebSocketSharp;
+using WebSocketSharp.Server;
+#endif
+
+namespace LiteNetLibManager
+{
+#if !UNITY_WEBGL || UNITY_EDITOR
+    public class WSBehavior : WebSocketBehavior
+    {
+        public long connectionId { get; }
+        private readonly Queue<TransportEventData> eventQueue;
+
+        public WSBehavior(long connectionId, Queue<TransportEventData> eventQueue)
+        {
+            this.connectionId = connectionId;
+            this.eventQueue = eventQueue;
+        }
+
+        protected override void OnOpen()
+        {
+            base.OnOpen();
+            eventQueue.Enqueue(new TransportEventData()
+            {
+                type = ENetworkEvent.ConnectEvent,
+                connectionId = connectionId,
+            });
+        }
+
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            base.OnMessage(e);
+            eventQueue.Enqueue(new TransportEventData()
+            {
+                type = ENetworkEvent.DataEvent,
+                connectionId = connectionId,
+                reader = new NetDataReader(e.RawData),
+            });
+        }
+
+        protected override void OnError(ErrorEventArgs e)
+        {
+            base.OnError(e);
+            eventQueue.Enqueue(new TransportEventData()
+            {
+                type = ENetworkEvent.ErrorEvent,
+            });
+        }
+
+        protected override void OnClose(CloseEventArgs e)
+        {
+            base.OnClose(e);
+            eventQueue.Enqueue(new TransportEventData()
+            {
+                type = ENetworkEvent.DisconnectEvent,
+                connectionId = connectionId,
+            });
+        }
+    }
+#endif
+}
--- Scripts/Transports/WebSocket/WebSocketTransport.cs
+++ Scripts/Transports/WebSocket/WebSocketTransport.cs
@@ -184,59 +184,5 @@ namespace LiteNetLibManager
             socketV4.Close();
             return port;
         }
-
-#if !UNITY_WEBGL || UNITY_EDITOR
-        private class WSBehavior : WebSocketBehavior
-        {
-            public long connectionId { get; }
-            private readonly Queue<TransportEventData> eventQueue;
-
-            public WSBehavior(long connectionId, Queue<TransportEventData> eventQueue)
-            {
-                this.connectionId = connectionId;
-                this.eventQueue = eventQueue;
-            }
-
-            protected override void OnOpen()
-            {
-                base.OnOpen();
-                eventQueue.Enqueue(new TransportEventData()
-                {
-                    type = ENetworkEvent.ConnectEvent,
-                    connectionId = connectionId,
-                });
-            }
-
-            protected override void OnMessage(MessageEventArgs e)
-            {
-                base.OnMessage(e);
-                eventQueue.Enqueue(new TransportEventData()
-                {
-                    type = ENetworkEvent.DataEvent,
-                    connectionId = connectionId,
-                    reader = new NetDataReader(e.RawData),
-                });
-            }
-
-            protected override void OnError(ErrorEventArgs e)
-            {
-                base.OnError(e);
-                eventQueue.Enqueue(new TransportEventData()
-                {
-                    type = ENetworkEvent.ErrorEvent,
-                });
-            }
-
-            protected override void OnClose(CloseEventArgs e)
-            {
-                base.OnClose(e);
-                eventQueue.Enqueue(new TransportEventData()
-                {
-                    type = ENetworkEvent.DisconnectEvent,
-                    connectionId = connectionId,
-                });
-            }
-        }
-#endif
     }
 }
