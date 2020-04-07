3ff8b5f85822065fb6767126b3086feb81712d81
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2020-04-07 10:02:00 +0700

    Avoid null event

--- Scripts/Logging.cs
+++ Scripts/Logging.cs
@@ -16,7 +16,8 @@ namespace LiteNetLibManager
 #if UNITY_EDITOR
             Debug.Log("[" + tag + "] " + message.ToString());
 #endif
-            onLog.Invoke(LogType.Log, tag, message.ToString());
+            if (onLog != null)
+                onLog.Invoke(LogType.Log, tag, message.ToString());
         }
 
         public static void LogError(string tag, object message)
@@ -26,7 +27,8 @@ namespace LiteNetLibManager
 #if UNITY_EDITOR
             Debug.LogError("[" + tag + "] " + message.ToString());
 #endif
-            onLog.Invoke(LogType.Log, tag, message.ToString());
+            if (onLog != null)
+                onLog.Invoke(LogType.Log, tag, message.ToString());
         }
 
         public static void LogWarning(string tag, object message)
@@ -36,7 +38,8 @@ namespace LiteNetLibManager
 #if UNITY_EDITOR
             Debug.LogWarning("[" + tag + "] " + message.ToString());
 #endif
-            onLog.Invoke(LogType.Warning, tag, message.ToString());
+            if (onLog != null)
+                onLog.Invoke(LogType.Warning, tag, message.ToString());
         }
 
         public static void LogException(string tag, System.Exception ex)
@@ -46,7 +49,8 @@ namespace LiteNetLibManager
 #if UNITY_EDITOR
             Debug.LogException(ex);
 #endif
-            onLog.Invoke(LogType.Exception, tag, ex.ToString());
+            if (onLog != null)
+                onLog.Invoke(LogType.Exception, tag, ex.ToString());
         }
 
         public static void Log(object message)
@@ -56,7 +60,8 @@ namespace LiteNetLibManager
 #if UNITY_EDITOR
             Debug.Log(message);
 #endif
-            onLog.Invoke(LogType.Log, NoTag, message.ToString());
+            if (onLog != null)
+                onLog.Invoke(LogType.Log, NoTag, message.ToString());
         }
 
         public static void LogError(object message)
@@ -66,7 +71,8 @@ namespace LiteNetLibManager
 #if UNITY_EDITOR
             Debug.LogError(message);
 #endif
-            onLog.Invoke(LogType.Error, NoTag, message.ToString());
+            if (onLog != null)
+                onLog.Invoke(LogType.Error, NoTag, message.ToString());
         }
 
         public static void LogWarning(object message)
@@ -76,7 +82,8 @@ namespace LiteNetLibManager
 #if UNITY_EDITOR
             Debug.LogWarning(message);
 #endif
-            onLog.Invoke(LogType.Warning, NoTag, message.ToString());
+            if (onLog != null)
+                onLog.Invoke(LogType.Warning, NoTag, message.ToString());
         }
 
         public static void LogException(System.Exception ex)
@@ -86,7 +93,8 @@ namespace LiteNetLibManager
 #if UNITY_EDITOR
             Debug.LogException(ex);
 #endif
-            onLog.Invoke(LogType.Exception, NoTag, ex.ToString());
+            if (onLog != null)
+                onLog.Invoke(LogType.Exception, NoTag, ex.ToString());
         }
     }
 }
