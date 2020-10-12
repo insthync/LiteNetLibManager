0a9453b083d16e7e7d06202c4081e8db552ec0f8
Insthync <ittipon.bay@gmail.com>
2020-10-12 07:27:35 +0700

    Add Tcp Transport, May use it with inter-server connections (For example map-server connect to login-server)

---
+++ Scripts/Transports/TcpTransport/TcpTransportFactory.cs
@@ -0,0 +1,12 @@
+﻿namespace LiteNetLibManager
+{
+    public class TcpTransportFactory : BaseTransportFactory
+    {
+        public override bool CanUseWithWebGL { get { return false; } }
+
+        public override ITransport Build()
+        {
+            return new TcpTransport();
+        }
+    }
+}
