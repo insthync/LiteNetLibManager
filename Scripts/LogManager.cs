927901333c39d71eb5eacd5b557d50ae6fcff896
Insthync <ittipon.bay@gmail.com>
2021-06-09 10:40:39 +0700

    Change logging system to use ZLogger

---
+++ Scripts/LogManager.cs
@@ -0,0 +1,95 @@
+﻿using Cysharp.Text;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Buffers;
+using System.Collections.Generic;
+using ZLogger;
+
+namespace LiteNetLibManager
+{
+    public static partial class LogManager
+    {
+        const int RollSizeKB = 1024;
+        static ILogger globalLogger;
+        static ILoggerFactory loggerFactory;
+        public static ILoggerFactory LoggerFactory
+        {
+            get => loggerFactory;
+            set
+            {
+                if (loggerFactory != null)
+                    loggerFactory.Dispose();
+                LoggerByTypes.Clear();
+                LoggerByTags.Clear();
+                loggerFactory = value;
+                globalLogger = loggerFactory.CreateLogger("Global");
+            }
+        }
+        static readonly Dictionary<string, ILogger> LoggerByTypes = new Dictionary<string, ILogger>();
+        static readonly Dictionary<string, ILogger> LoggerByTags = new Dictionary<string, ILogger>();
+
+        // Setup on first called GetLogger<T>.
+        static LogManager()
+        {
+            // Standard LoggerFactory does not work on IL2CPP,
+            // But you can use ZLogger's UnityLoggerFactory instead,
+            // it works on IL2CPP, all platforms(includes mobile).
+            LoggerFactory = UnityLoggerFactory.Create(builder =>
+            {
+                // or more configuration, you can use builder.AddFilter
+                builder.SetMinimumLevel(LogLevel.Trace);
+
+                // AddZLoggerUnityDebug is only available for Unity, it send log to UnityEngine.Debug.Log.
+                // LogLevels are translate to
+                // * Trace/Debug/Information -> LogType.Log
+                // * Warning/Critical -> LogType.Warning
+                // * Error without Exception -> LogType.Error
+                // * Error with Exception -> LogException
+                builder.AddZLoggerUnityDebug();
+            });
+
+            UnityEngine.Application.quitting += () =>
+            {
+                // when quit, flush unfinished log entries.
+                if (loggerFactory != null)
+                    loggerFactory.Dispose();
+            };
+        }
+
+        public static void PrefixFormatterConfigure(IBufferWriter<byte> writer, LogInfo info)
+        {
+            switch (info.LogLevel)
+            {
+                case LogLevel.Trace:
+                case LogLevel.Debug:
+                case LogLevel.Information:
+                    ZString.Utf8Format(writer, " INFO {0} [{1}] - ", info.CategoryName, info.Timestamp.ToLocalTime().ToString("yyyy-MM-dd HH:mm:ss"));
+                    break;
+                case LogLevel.Warning:
+                case LogLevel.Critical:
+                    ZString.Utf8Format(writer, " WARN {0} [{1}] - ", info.CategoryName, info.Timestamp.ToLocalTime().ToString("yyyy-MM-dd HH:mm:ss"));
+                    break;
+                case LogLevel.Error:
+                    ZString.Utf8Format(writer, "ERROR {0} [{1}] - ", info.CategoryName, info.Timestamp.ToLocalTime().ToString("yyyy-MM-dd HH:mm:ss"));
+                    break;
+            }
+        }
+
+        public static ILogger Logger => globalLogger;
+
+        public static ILogger<T> GetLogger<T>() where T : class
+        {
+            string typeFullName = typeof(T).FullName;
+            if (!LoggerByTypes.ContainsKey(typeFullName))
+                LoggerByTypes.Add(typeFullName, LoggerFactory.CreateLogger<T>());
+            return LoggerByTypes[typeFullName] as ILogger<T>;
+        }
+
+        public static ILogger GetLogger(string tag)
+        {
+            if (!LoggerByTags.ContainsKey(tag))
+                LoggerByTags.Add(tag, LoggerFactory.CreateLogger(tag));
+            return LoggerByTags[tag];
+        }
+    }
+}
--- Scripts/Logging.cs
+++ Scripts/Logging.cs
@@ -1,91 +1,82 @@
-﻿using Cysharp.Threading.Tasks;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using Microsoft.Extensions.Logging;
 
 namespace LiteNetLibManager
 {
     public static class Logging
     {
-        public static System.Action<LogType, string, string> onLog;
-        public const string NoTag = "No Tag";
+        public const string NoTag = "(No Tag)";
 
         public static void Log(string tag, object message)
         {
             if (message == null)
                 return;
-            InvokeOnLog(LogType.Log, tag, message.ToString()).Forget();
+            InvokeOnLog(UnityEngine.LogType.Log, tag, message.ToString());
         }
 
         public static void LogError(string tag, object message)
         {
             if (message == null)
                 return;
-            InvokeOnLog(LogType.Error, tag, message.ToString()).Forget();
+            InvokeOnLog(UnityEngine.LogType.Error, tag, message.ToString());
         }
 
         public static void LogWarning(string tag, object message)
         {
             if (message == null)
                 return;
-            InvokeOnLog(LogType.Warning, tag, message.ToString()).Forget();
+            InvokeOnLog(UnityEngine.LogType.Warning, tag, message.ToString());
         }
 
         public static void LogException(string tag, System.Exception ex)
         {
             if (ex == null)
                 return;
-            InvokeOnLog(LogType.Exception, tag, ex.ToString()).Forget();
+            InvokeOnLog(UnityEngine.LogType.Exception, tag, ex.ToString());
         }
 
         public static void Log(object message)
         {
             if (message == null)
                 return;
-            InvokeOnLog(LogType.Log, NoTag, message.ToString()).Forget();
+            InvokeOnLog(UnityEngine.LogType.Log, NoTag, message.ToString());
         }
 
         public static void LogError(object message)
         {
             if (message == null)
                 return;
-            InvokeOnLog(LogType.Error, NoTag, message.ToString()).Forget();
+            InvokeOnLog(UnityEngine.LogType.Error, NoTag, message.ToString());
         }
 
         public static void LogWarning(object message)
         {
             if (message == null)
                 return;
-            InvokeOnLog(LogType.Warning, NoTag, message.ToString()).Forget();
+            InvokeOnLog(UnityEngine.LogType.Warning, NoTag, message.ToString());
         }
 
         public static void LogException(System.Exception ex)
         {
             if (ex == null)
                 return;
-            InvokeOnLog(LogType.Exception, NoTag, ex.ToString()).Forget();
+            InvokeOnLog(UnityEngine.LogType.Exception, NoTag, ex.ToString());
         }
 
-        private static async UniTaskVoid InvokeOnLog(LogType logType, string tag, string text)
+        private static void InvokeOnLog(UnityEngine.LogType logType, string tag, string text)
         {
-            await UniTask.SwitchToMainThread();
-#if UNITY_EDITOR
             switch (logType)
             {
-                case LogType.Log:
-                    Debug.Log(text);
+                case UnityEngine.LogType.Log:
+                    LogManager.GetLogger(tag).LogInformation(text);
                     break;
-                case LogType.Error:
-                case LogType.Exception:
-                    Debug.LogError(text);
+                case UnityEngine.LogType.Error:
+                case UnityEngine.LogType.Exception:
+                    LogManager.GetLogger(tag).LogError(text);
                     break;
-                case LogType.Warning:
-                    Debug.LogWarning(text);
+                case UnityEngine.LogType.Warning:
+                    LogManager.GetLogger(tag).LogWarning(text);
                     break;
             }
-#endif
-            if (onLog != null)
-                onLog.Invoke(logType, tag, text);
         }
     }
 }
