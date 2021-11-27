70b7aa1e7a8209b04bf4887e3cadeb9bf4c50111
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2021-11-28 05:19:28 +0700

    Fix wrong data

--- Scripts/LiteNetLibClient.cs
+++ Scripts/LiteNetLibClient.cs
@@ -106,7 +106,7 @@ namespace LiteNetLibManager
             if (!CreateAndWriteRequest(writer, requestType, request, responseDelegate, millisecondsTimeout, extraSerializer))
                 return false;
             // Send request to server, so connection id will not being used
-            SendMessage(-1, DeliveryMethod.ReliableUnordered, writer.Data);
+            SendMessage(0, DeliveryMethod.ReliableUnordered, writer.Data);
             return true;
         }
 
@@ -125,7 +125,7 @@ namespace LiteNetLibManager
                 done = true;
             }, millisecondsTimeout, extraSerializer);
             // Send request to server, so connection id will not being used
-            SendMessage(-1, DeliveryMethod.ReliableUnordered, writer.Data);
+            SendMessage(0, DeliveryMethod.ReliableUnordered, writer.Data);
             // Wait for response
             do
             {
