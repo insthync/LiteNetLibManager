d6854692b8ef7f5dc987f5a7ed8be36111aae5f7
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2023-04-27 00:45:20 +0700

    Revert "feat: delete codes for Unity (PackedVariables)"
    This reverts commit e964fdde578af97a0555ed4cce30184a9608566b.

---
+++ Scripts/PackedVariables/DirectionVector3.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using LiteNetLib.Utils;
+
+public struct DirectionVector3 : INetSerializable
+{
+    public static implicit operator DirectionVector3(Vector3 value) { return new DirectionVector3(value); }
+    public static implicit operator Vector3(DirectionVector3 value) { return new Vector3((float)value.x / 100f, (float)value.y / 100f, (float)value.z / 100f); }
+
+    public sbyte x;
+    public sbyte y;
+    public sbyte z;
+
+    public DirectionVector3(Vector3 vector3)
+    {
+        x = (sbyte)(vector3.x * 100);
+        y = (sbyte)(vector3.y * 100);
+        z = (sbyte)(vector3.z * 100);
+    }
+
+    public void Serialize(NetDataWriter writer)
+    {
+        writer.Put(x);
+        writer.Put(y);
+        writer.Put(z);
+    }
+
+    public void Deserialize(NetDataReader reader)
+    {
+        x = reader.GetSByte();
+        y = reader.GetSByte();
+        z = reader.GetSByte();
+    }
+}
