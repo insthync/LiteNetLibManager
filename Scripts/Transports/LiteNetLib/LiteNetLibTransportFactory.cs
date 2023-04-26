a9afa87276ece15abc49cf483afac6f43d9e2fee
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2023-04-27 01:02:40 +0700

    feat: delete codes for Unity, changes some for NET7 (Transport)

--- Scripts/Transports/LiteNetLib/LiteNetLibTransportFactory.cs
+++ Scripts/Transports/LiteNetLib/LiteNetLibTransportFactory.cs
@@ -5,9 +5,7 @@ namespace LiteNetLibManager
     public class LiteNetLibTransportFactory : BaseTransportFactory
     {
         public string connectKey = "SampleConnectKey";
-        [Range(1, 64)]
         public byte clientDataChannelsCount = 16;
-        [Range(1, 64)]
         public byte serverDataChannelsCount = 16;
 
         public override ITransport Build()
