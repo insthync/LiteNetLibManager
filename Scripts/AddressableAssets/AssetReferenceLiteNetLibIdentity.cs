d37ffc4d9e5dd2c4d9e8e3048ebc6d25d6f4ae1d
insthync <ittipon.bay@gmail.com>
2024-03-26 18:08:51 +0700

    feat: set hash asset ID to asset refs, so we can use it to spawn proper objects

--- Scripts/AddressableAssets/AssetReferenceLiteNetLibIdentity.cs
+++ Scripts/AddressableAssets/AssetReferenceLiteNetLibIdentity.cs
@@ -1,10 +1,66 @@
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 namespace LiteNetLibManager
 {
     [System.Serializable]
     public class AssetReferenceLiteNetLibIdentity : AssetReferenceComponent<LiteNetLibIdentity>
     {
-        public AssetReferenceLiteNetLibIdentity(string guid) : base(guid)
+        [SerializeField]
+        private int hashAssetId;
+
+        public int HashAssetId
         {
+            get { return hashAssetId; }
         }
+
+#if UNITY_EDITOR
+        public AssetReferenceLiteNetLibIdentity(LiteNetLibIdentity identity) : base(AssetDatabase.AssetPathToGUID(AssetDatabase.GetAssetPath(identity)))
+        {
+            if (identity != null)
+            {
+                hashAssetId = identity.HashAssetId;
+                Debug.Log($"[AssetReferenceLiteNetLibIdentity] Set `hashAssetId` to `{hashAssetId}`, name: {identity.name}");
+            }
+            else
+            {
+                hashAssetId = 0;
+                Debug.LogWarning($"[AssetReferenceLiteNetLibIdentity] Cannot find identity, so set `hashAssetId` to `0`");
+            }
+        }
+
+        public override bool ValidateAsset(string path)
+        {
+            return ValidateAsset(AssetDatabase.LoadAssetAtPath<LiteNetLibIdentity>(path));
+        }
+
+        public override bool ValidateAsset(Object obj)
+        {
+            return (obj != null) && (obj is LiteNetLibIdentity);
+        }
+
+        public override bool SetEditorAsset(Object value)
+        {
+            if (!base.SetEditorAsset(value))
+            {
+                return false;
+            }
+
+            if ((value is GameObject gameObject) && gameObject.TryGetComponent(out LiteNetLibIdentity identity))
+            {
+                hashAssetId = identity.HashAssetId;
+                Debug.Log($"[AssetReferenceLiteNetLibIdentity] Set `hashAssetId` to `{hashAssetId}` when set editor asset: `{value.name}`");
+                return true;
+            }
+            else
+            {
+                hashAssetId = 0;
+                Debug.LogWarning($"[AssetReferenceLiteNetLibIdentity] Cannot find identity or not proper object's type, so set `hashAssetId` to `0`");
+                return false;
+            }
+        }
+#endif
     }
 }
