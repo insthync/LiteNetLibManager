a0668d9a79b4dbf96fa310b26cba4b311adfaa81
insthync <ittipon.bay@gmail.com>
2024-03-26 18:35:30 +0700

    feat: add ref to litenetlib behaviour

---
+++ Scripts/AddressableAssets/AssetReferenceLiteNetLibBehaviour.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace LiteNetLibManager
+{
+    [System.Serializable]
+    public class AssetReferenceLiteNetLibBehaviour : AssetReferenceComponent<LiteNetLibBehaviour>
+    {
+        [SerializeField]
+        private int hashAssetId;
+
+        public int HashAssetId
+        {
+            get { return hashAssetId; }
+        }
+
+#if UNITY_EDITOR
+        public AssetReferenceLiteNetLibBehaviour(LiteNetLibBehaviour behaviour) : base(AssetDatabase.AssetPathToGUID(AssetDatabase.GetAssetPath(behaviour)))
+        {
+            if (behaviour != null && behaviour.TryGetComponent(out LiteNetLibIdentity identity))
+            {
+                hashAssetId = identity.HashAssetId;
+                Debug.Log($"[AssetReferenceLiteNetLibBehaviour] Set `hashAssetId` to `{hashAssetId}`, name: {behaviour.name}");
+            }
+            else
+            {
+                hashAssetId = 0;
+                Debug.LogWarning($"[AssetReferenceLiteNetLibBehaviour] Cannot find behaviour, so set `hashAssetId` to `0`");
+            }
+        }
+
+        public override bool SetEditorAsset(Object value)
+        {
+            if (!base.SetEditorAsset(value))
+            {
+                return false;
+            }
+
+            if ((value is GameObject gameObject) && gameObject.TryGetComponent(out LiteNetLibIdentity identity))
+            {
+                hashAssetId = identity.GetComponent<LiteNetLibIdentity>().HashAssetId;
+                Debug.Log($"[AssetReferenceLiteNetLibBehaviour] Set `hashAssetId` to `{hashAssetId}` when set editor asset: `{value.name}`");
+                return true;
+            }
+            else
+            {
+                hashAssetId = 0;
+                Debug.LogWarning($"[AssetReferenceLiteNetLibBehaviour] Cannot find behaviour or not proper object's type, so set `hashAssetId` to `0`");
+                return false;
+            }
+        }
+#endif
+    }
+}
