dd61caabf620fec7f95b1a09b04fbd740c5f7f55
insthync <ittipon.bay@gmail.com>
2024-04-08 13:43:58 +0700

    improve: add hint for the event

--- Scripts/AddressableAssets/Events/AddressableAssetTotalProgressEvent.cs
+++ Scripts/AddressableAssets/Events/AddressableAssetTotalProgressEvent.cs
@@ -2,6 +2,9 @@ using UnityEngine.Events;
 
 namespace LiteNetLibManager
 {
+    /// <summary>
+    /// Loaded Count, Total Count
+    /// </summary>
     [System.Serializable]
     public class AddressableAssetTotalProgressEvent : UnityEvent<int, int>
     {
