eb5bee634264eaaaf59b491c75f446de59be5084
insthync <ittipon.bay@gmail.com>
2024-06-23 19:11:07 +0700

    feat: clear loading scenes when start a game

---
+++ Scripts/GameApi/LiteNetLibGameInitializer.cs
@@ -0,0 +1,11 @@
+﻿namespace LiteNetLibManager
+{
+    public static class LiteNetLibGameInitializer
+    {
+        [UnityEngine.RuntimeInitializeOnLoadMethod(UnityEngine.RuntimeInitializeLoadType.BeforeSceneLoad)]
+        public static void Initialize()
+        {
+            LiteNetLibGameManager.LoadingServerScenes.Clear();
+        }
+    }
+}
