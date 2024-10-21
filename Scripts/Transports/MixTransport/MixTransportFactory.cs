746bff2cea8cbac08cb3e7583f0908facb24180e
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2024-10-22 01:17:45 +0700

    feat: rework on mix transport

--- Scripts/Transports/MixTransport/MixTransportFactory.cs
+++ Scripts/Transports/MixTransport/MixTransportFactory.cs
@@ -5,6 +5,8 @@ namespace LiteNetLibManager
     public class MixTransportFactory : BaseTransportFactory, IWebSocketTransportFactory
     {
         [SerializeField]
+        private bool shouldUseWebSocket = false;
+        [SerializeField]
         private string connectKey = "SampleConnectKey";
         [SerializeField]
         private int webSocketPortOffset = 100;
@@ -14,19 +16,24 @@ namespace LiteNetLibManager
         private string webSocketCertificateFilePath = string.Empty;
         [SerializeField]
         private string webSocketCertificatePassword = string.Empty;
+        [SerializeField]
+        private string webSocketCertificateBase64String = string.Empty;
         [Range(1, 64)]
         [SerializeField]
         private byte clientDataChannelsCount = 16;
         [Range(1, 64)]
         [SerializeField]
         private byte serverDataChannelsCount = 16;
+        public bool ShouldUseWebSocket { get { return shouldUseWebSocket; } set { shouldUseWebSocket = value; } }
         public bool Secure { get { return webSocketSecure; } set { webSocketSecure = value; } }
         public string CertificateFilePath { get { return webSocketCertificateFilePath; } set { webSocketCertificateFilePath = value; } }
         public string CertificatePassword { get { return webSocketCertificatePassword; } set { webSocketCertificatePassword = value; } }
+        public string CertificateBase64String { get { return webSocketCertificateBase64String; } set { webSocketCertificateBase64String = value; } }
 
         public override ITransport Build()
         {
-            return new MixTransport(connectKey, webSocketPortOffset, webSocketSecure, webSocketCertificateFilePath, webSocketCertificatePassword, clientDataChannelsCount, serverDataChannelsCount);
+            return new MixTransport(shouldUseWebSocket, connectKey, clientDataChannelsCount, serverDataChannelsCount,
+                webSocketPortOffset, webSocketSecure, webSocketCertificateFilePath, webSocketCertificatePassword, webSocketCertificateBase64String);
         }
     }
 }
