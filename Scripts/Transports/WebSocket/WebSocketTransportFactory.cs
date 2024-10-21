6e8991ee47744e7d982eb316421d48a93a1f4d3e
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2024-10-21 21:24:01 +0700

    feat: change web socket transport implementation
    # Conflicts:
    #	Scripts/Transports/WebSocket/WebSocketTransportFactory.cs
    #	Scripts/Transports/WebSocket/WsClientWrapper.cs
    #	Scripts/Transports/WebSocket/WsTransportClient.cs
    #	Scripts/Transports/WebSocket/WsTransportSession.cs
    #	Scripts/Transports/WebSocket/WssTransportClient.cs
    #	Scripts/Transports/WebSocket/WssTransportSession.cs

--- Scripts/Transports/WebSocket/WebSocketTransportFactory.cs
+++ Scripts/Transports/WebSocket/WebSocketTransportFactory.cs
@@ -2,13 +2,18 @@
 {
     public class WebSocketTransportFactory : BaseTransportFactory, IWebSocketTransportFactory
     {
-        public bool Secure { get; set; }
-        public string CertificateFilePath { get; set; } = string.Empty;
-        public string CertificatePassword { get; set; } = string.Empty;
+        public bool secure = false;
+        public string certificateFilePath = string.Empty;
+        public string certificatePassword = string.Empty;
+        public string certificateBase64String = string.Empty;
+        public bool Secure { get { return secure; } set { secure = value; } }
+        public string CertificateFilePath { get { return certificateFilePath; } set { certificateFilePath = value; } }
+        public string CertificatePassword { get { return certificatePassword; } set { certificatePassword = value; } }
+        public string CertificateBase64String { get { return certificateBase64String; } set { certificateBase64String = value; } }
 
         public override ITransport Build()
         {
-            return new WebSocketTransport(Secure, CertificateFilePath, CertificatePassword);
+            return new WebSocketTransport(secure, certificateFilePath, certificatePassword, certificateBase64String);
         }
     }
 }
