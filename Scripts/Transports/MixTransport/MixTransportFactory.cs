e0a39151f374db52cedfd0cd6c7a723124e7ab27
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2024-10-21 21:24:11 +0700

    feat: change web socket transport implementation

--- Scripts/Transports/MixTransport/MixTransportFactory.cs
+++ Scripts/Transports/MixTransport/MixTransportFactory.cs
@@ -1,19 +1,26 @@
-﻿namespace LiteNetLibManager
+﻿using UnityEngine;
+
+namespace LiteNetLibManager
 {
     public class MixTransportFactory : BaseTransportFactory, IWebSocketTransportFactory
     {
-        public string ConnectKey { get; set; } = "SampleConnectKey";
-        public int WebSocketPortOffset { get; set; } = 100;
-        public byte ClientDataChannelsCount { get; set; } = 16;
-        public byte ServerDataChannelsCount { get; set; } = 16;
-        public bool Secure { get; set; }
-        public string CertificateFilePath { get; set; } = string.Empty;
-        public string CertificatePassword { get; set; } = string.Empty;
+        public string connectKey = "SampleConnectKey";
+        public int webSocketPortOffset = 100;
+        public bool webSocketSecure = false;
+        public string webSocketCertificateFilePath = string.Empty;
+        public string webSocketCertificatePassword = string.Empty;
+        [Range(1, 64)]
+        public byte clientDataChannelsCount = 16;
+        [Range(1, 64)]
+        public byte serverDataChannelsCount = 16;
+        public bool Secure { get { return webSocketSecure; } set { webSocketSecure = value; } }
+        public string CertificateFilePath { get { return webSocketCertificateFilePath; } set { webSocketCertificateFilePath = value; } }
+        public string CertificatePassword { get { return webSocketCertificatePassword; } set { webSocketCertificatePassword = value; } }
 
 
         public override ITransport Build()
         {
-            return new MixTransport(ConnectKey, WebSocketPortOffset, Secure, CertificateFilePath, CertificatePassword, ClientDataChannelsCount, ServerDataChannelsCount);
+            return new MixTransport(connectKey, webSocketPortOffset, webSocketSecure, webSocketCertificateFilePath, webSocketCertificatePassword, clientDataChannelsCount, serverDataChannelsCount);
         }
     }
 }
