494991fb822e66ef103812053677a826afe74cac
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2025-02-10 11:21:46 +0700

    Revert "feat: increase direction vector accuracy"
    This reverts commit 5e24879f624898e425f8a1f4811cc611a3e2ef9a.

--- Scripts/PackedVariables/DirectionVector2.cs
+++ Scripts/PackedVariables/DirectionVector2.cs
@@ -5,27 +5,27 @@ public struct DirectionVector2 : INetSerializable
 {
     public static implicit operator DirectionVector2(Vector2 value) { return new DirectionVector2(value); }
     public static implicit operator DirectionVector2(Vector3 value) { return new DirectionVector2(value); }
-    public static implicit operator Vector2(DirectionVector2 value) { return new Vector2((float)value.x / 10000f, (float)value.y / 10000f); }
-    public static implicit operator Vector3(DirectionVector2 value) { return new Vector2((float)value.x / 10000f, (float)value.y / 10000f); }
+    public static implicit operator Vector2(DirectionVector2 value) { return new Vector2((float)value.x / 100f, (float)value.y / 100f); }
+    public static implicit operator Vector3(DirectionVector2 value) { return new Vector2((float)value.x / 100f, (float)value.y / 100f); }
 
-    public short x;
-    public short y;
+    public sbyte x;
+    public sbyte y;
 
     public DirectionVector2(Vector2 vector2)
     {
-        x = (short)(vector2.x * 10000);
-        y = (short)(vector2.y * 10000);
+        x = (sbyte)(vector2.x * 100);
+        y = (sbyte)(vector2.y * 100);
     }
 
     public void Serialize(NetDataWriter writer)
     {
-        writer.PutPackedShort(x);
-        writer.PutPackedShort(y);
+        writer.Put(x);
+        writer.Put(y);
     }
 
     public void Deserialize(NetDataReader reader)
     {
-        x = reader.GetPackedShort();
-        y = reader.GetPackedShort();
+        x = reader.GetSByte();
+        y = reader.GetSByte();
     }
 }
