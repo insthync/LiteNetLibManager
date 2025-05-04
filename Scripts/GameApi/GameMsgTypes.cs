64591a72cf156b8e1dec3f96e4a074092bfcc331
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2025-05-04 10:16:06 +0700

    chore: update packet types

--- Scripts/GameApi/GameMsgTypes.cs
+++ Scripts/GameApi/GameMsgTypes.cs
@@ -5,15 +5,13 @@
         public const ushort Request = 0;
         public const ushort Response = 1;
         public const ushort CallFunction = 2;
-        public const ushort ServerSpawnObjects = 3;
-        public const ushort ServerDestroyObjects = 4;
-        public const ushort SyncElements = 5;
-        public const ushort ServerError = 6;
-        public const ushort ServerSceneChange = 7;
-        public const ushort ServerSetObjectOwner = 8;
-        public const ushort Ping = 9;
-        public const ushort Pong = 10;
-        public const ushort Disconnect = 11;
-        public const ushort Highest = 11;
+        public const ushort SyncElements = 3;
+        public const ushort ServerError = 4;
+        public const ushort ServerSceneChange = 5;
+        public const ushort ServerSetObjectOwner = 6;
+        public const ushort Ping = 7;
+        public const ushort Pong = 8;
+        public const ushort Disconnect = 9;
+        public const ushort Highest = 9;
     }
 }
